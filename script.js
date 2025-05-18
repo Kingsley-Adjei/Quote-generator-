66895eb4d2f8e472475a645f0891c9578d84b660
YUNGZEUS <kingkadjei002@email.com>
2025-05-18 04:26:59 +0000

    loader added

--- script.js
+++ script.js
@@ -3,10 +3,24 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 // let apiQuotes = [];
 // Show New Quote
+
+// Show Loading
+function loading(){
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+// Hide Loading
+function complete(){
+  quoteContainer.hidden = false;
+  loader.hidden = true
+}
+
 function newQuote(){
+  loading();
   // Pick a random quote from apiQuotes array
   const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
   // Check if Author field is blank and replace it with 'Unknown'
@@ -21,10 +35,13 @@ function newQuote(){
   }else {
     quoteText.classList.remove('long-quote')
   }
+  // Set Quote, Hide Loader
   quoteText.textContent = quote.text;
+  complete();
 }
 // Get Quotes From API
 // async function getQuotes() {
+//   loading();
 //   const apiUrl = 'https://type.fit/api/quotes'
 //   try {
 //     const response = await fetch(apiUrl);
@@ -37,6 +54,8 @@ function newQuote(){
 
 // On Load
 // getQuotes();
+newQuote();
+
 
 // Tweet Quote
 function tweetQuote() {
@@ -47,5 +66,3 @@ function tweetQuote() {
 // Event Listerners
 newQuoteBtn.addEventListener('click', newQuote);
 twitterBtn.addEventListener('click',tweetQuote);
-
-newQuote();
