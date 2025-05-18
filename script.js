be47a79705b50e389f6f9b1afd62dc22451e6abb
YUNGZEUS <kingkadjei002@email.com>
2025-05-18 03:34:34 +0000

    functioning buttons

--- script.js
+++ script.js
@@ -9,7 +9,18 @@ const newQuoteBtn = document.getElementById('new-quote');
 function newQuote(){
   // Pick a random quote from apiQuotes array
   const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
-  authorText.textContent = quote.author;
+  // Check if Author field is blank and replace it with 'Unknown'
+  if(!quote.author){
+    authorText.textContent = 'Unknown';
+  }else {
+    authorText.textContent = quote.author;
+  }
+  //  Check Quote length to determine styling
+  if (quote.text.length > 120) {
+    quoteText.classList.add('long-quote');
+  }else {
+    quoteText.classList.remove('long-quote')
+  }
   quoteText.textContent = quote.text;
 }
 // Get Quotes From API
@@ -26,4 +37,15 @@ function newQuote(){
 
 // On Load
 // getQuotes();
+
+// Tweet Quote
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+  window.open(twitterUrl, '_blank');
+}
+
+// Event Listerners
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click',tweetQuote);
+
 newQuote();
